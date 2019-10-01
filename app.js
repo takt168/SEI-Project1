4233a76cfc0a20901007770383bc4ee2ccca3cd1
Tak Tsukamoto <takt168@gmail.com>
2019-09-30 23:06:09 -0400

    incremental checkin for the night

--- app.js
+++ app.js
@@ -7,26 +7,27 @@ const altCardDiv = document.querySelector(`#alt-card`);
 const evolutionCardBanner = document.querySelector(`#evolution-card-banner`);
 const evolutionCardDiv = document.querySelector(`#evolution-card`);
 
+const evolutionCard1 = document.querySelector(`#evolution-card-1`);
+const evolutionCard2 = document.querySelector(`#evolution-card-2`);
+const evolutionCard3 = document.querySelector(`#evolution-card-3`);
 
-//function to clear html element of all child elements
-const clearChildElements = (elem) => {
-  while (elem.hasChildNodes()) {
-    elem.removeChild(elem.childNodes[0]);
-  }
-}
 
 //function to capitalize first letter of a String
-const capitalize = (s) => {
+const capitalizeEachWord = (s) => {
   if (typeof s !== 'string') {
     return '';
   } else {
-    return s.charAt(0).toUpperCase() + s.slice(1);
+    let splitStr = s.toLowerCase().split(' ');
+    for (var i = 0; i < splitStr.length; i++) {
+      splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+    }
+    return splitStr.join(' ');
   }
 }
 
 searchButton.addEventListener(`click`, async () => {
 
-  const characterName = capitalize(document.querySelector(`#character-name`).value);
+  const characterName = capitalizeEachWord(document.querySelector(`#character-name`).value);
   const characterNameFormatted = characterName.split(` `).join(`+`);
 
   const response = await axios.get(`${apiCall}name=${characterNameFormatted}`);
@@ -38,21 +39,52 @@ searchButton.addEventListener(`click`, async () => {
   cardArray.forEach(element => {
     console.log(element.id);
     console.log(element.name);
-    console.log(element.imageUrlHiRes);
+    //console.log(element.imageUrlHiRes);
+    console.log(element.subtype);
+    console.log(element.evolvesFrom);
     randomImg = element.imageUrlHiRes;
 
-    clearChildElements(mainCardBanner);
     mainCardBanner.innerHTML = `
     <h1 id="main-card-banner-title">${element.name}</h1>`;
 
-    clearChildElements(mainCardDiv);
     mainCardDiv.innerHTML = `
-      <img id="main-card-img" src="${element.imageUrlHiRes}">`;
+      <img id="main-card-img" src="${randomImg}">`;
 
+    //keep track of main card id, so you don't show it again in the alt card section
     mainCardId = element.id;
+    //update right card in evolution section.
+    if (element.subtype === `Basic`) {
+      evolutionCard1.innerHTML = ``;
+      evolutionCard1.innerHTML = `
+      <h2 class="evolution-card-header">Basic Card</h2>
+      <img class="evolution-cards" src="${randomImg}">`;
+    } else if (element.subtype == `Stage 1`) {
+      evolutionCard1.innerHTML = `
+      <h2 class="evolution-card-header">Basic Card</h2>
+      <img class="evolution-cards" src="Pokemon_Trading_Card_Game_cardback.jpg">`;
+      evolutionCard2.innerHTML = ``;
+      evolutionCard2.innerHTML = `
+      <h2 class="evolution-card-header">Stage 1 Card</h2>
+      <img class="evolution-cards" src="${randomImg}">`;
+    } else if (element.subtype == `Stage 2`) {
+      evolutionCard1.innerHTML = `
+      <h2 class="evolution-card-header">Basic Card</h2>
+      <img class="evolution-cards" src="Pokemon_Trading_Card_Game_cardback.jpg">`;
+      evolutionCard2.innerHTML = `
+      <h2 class="evolution-card-header">Stage 1 Card</h2>
+      <img class="evolution-cards" src="Pokemon_Trading_Card_Game_cardback.jpg">`;
+      evolutionCard3.innerHTML = `
+      <h2 class="evolution-card-header">Stage 2 Card</h2>
+      <img class="evolution-cards" src="${randomImg}">`;
+
+    }
+
+
   });
 
-  clearChildElements(altCardDiv);
+  //clear out altCardDiv
+  altCardDiv.innerHTML = ``;
+
   cardArray.forEach(element => {
     console.log(element.id);
     console.log(element.name);
@@ -60,11 +92,9 @@ searchButton.addEventListener(`click`, async () => {
     randomImg = element.imageUrlHiRes;
 
     if (mainCardId !== element.id) {
-      clearChildElements(altCardBanner);
       altCardBanner.innerHTML = `
       <h1 id="alt-card-banner-title">Alternate Cards for ${element.name}</h1>`;
 
-      //clearChildElements(altCardDiv);
       altCardDiv.innerHTML += `
         <img id="alt-card-img" src="${element.imageUrlHiRes}">`;
     }
